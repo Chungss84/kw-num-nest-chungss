6ff9021a867c84e558b0819fadbd54237665eef7
chungss <chungss0804@nate.com>
2024-11-03 21:46:20 +0900

    test2

--- lib.js
+++ lib.js
@@ -3,10 +3,22 @@ function avg(numbers) {
     return s / numbers.length;
 }
 function prime(num) {
-    return false;
+    if (num < 2) return false;
+    for (let divisor = 2; divisor * divisor <= num; divisor++) {
+        if (num % divisor === 0) return false;
+    }
+    return true;
 }
 function factorial(num) {
-    return -1;
+    if (num < 0) return -1; 
+    if (num > 15) return -1;
+    if (num === 0 || num === 1) return 1;
+
+    let result = 1;
+    for (let i = 2; i <= num; i++) {
+        result *= i;
+    }
+    return result;
 }
 module.exports = {
     avg,
--- lib.test.js
+++ lib.test.js
@@ -6,3 +6,23 @@ test("avg([3, 5, 7]) should be 5", () => {
 test("avg([-5, 5]) should be 0", () => {
     expect(lib.avg([-5, 5])).toBe(0);
 });
+
+test("prime(3) should be true", () => {
+    expect(lib.prime(3)).toBe(true);
+});
+test("prime(4) should be false", () => {
+    expect(lib.prime(4)).toBe(false);
+});
+
+test("factorial(5) should return 120", () => {
+    expect(lib.factorial(5)).toBe(120);
+});
+test("factorial(15) should return 1307674368000", () => {
+    expect(lib.factorial(15)).toBe(1307674368000);
+});
+test("factorial(-1) should return -1 for negative input", () => {
+    expect(lib.factorial(-1)).toBe(-1);
+});
+test("factorial(16) should return -1 for greater than 15 input", () => {
+    expect(lib.factorial(16)).toBe(-1);
+});
